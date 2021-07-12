78d7754ee81133ec9f49e9aad01a2a746230b92c
solurin <solurin@canisius.edu>
2021-07-12 16:08:29 +0000

    Please enter the commit message for your changes. Lines starting

---
+++ updated_locations.sh
@@ -0,0 +1,188 @@
+if [ ! -d "ana" ] | [ ! -e "ana" ]; then
+	ln -s /usr/local/clas-software/reconstruction/ana
+fi
+
+if [ ! -d "bankdefs" ] | [ ! -e "bankdefs" ]; then
+	ln -s /usr/local/clas-software/io/bankdefs
+fi
+
+if [ ! -d "bos" ] | [ ! -e "bos" ]; then
+        ln -s /usr/local/clas-software/io/bos
+fi
+
+if [ ! -d "bosio" ] | [ ! -e "bosio" ]; then
+        ln -s /usr/local/clas-software/io/bosio
+fi
+
+if [ ! -d "caldb" ] | [ ! -e "caldb" ]; then
+        ln -s /usr/local/clas-software/io/caldb
+fi
+
+if [ ! -d "c_bos_io" ] | [ ! -e "c_bos_io" ]; then
+        ln -s /usr/local/clas-software/io/c_bos_io
+fi
+
+if [ ! -d "cc" ] | [ ! -e "cc" ]; then
+        ln -s /usr/local/clas-software/detector/cc
+fi
+
+if [ ! -d "c_cern" ] | [ ! -e "c_cern" ]; then
+        ln -s /usr/local/clas-software/reconstruction/c_cern
+fi
+
+if [ ! -d "ced" ] | [ ! -e "ced" ]; then
+        ln -s /usr/local/clas-software/build/bin/ced
+fi
+
+if [ ! -d "ClasTool" ] | [ ! -e "ClasTool" ]; then
+        ln -s /usr/local/clas-software/analysis/ClasTool
+fi
+
+if [ ! -d "clasutil" ] | [ ! -e "clasutil" ]; then
+        ln -s /usr/local/clas-software/io/clasutil
+fi
+
+if [ ! -d "c_sql" ] | [ ! -e "c_sql" ]; then
+        ln -s /usr/local/clas-software/io/c_sql
+fi
+
+if [ ! -d "dc" ] | [ ! -e "dc" ]; then
+        ln -s /usr/local/clas-software/detector/dc
+fi
+
+if [ ! -d "ec" ] | [ ! -e "ec" ]; then
+        ln -s /usr/local/clas-software/detector/ec
+fi
+
+if [ ! -d "eloss" ] | [ ! -e "eloss" ]; then
+        ln -s /usr/local/clas-software/reconstruction/eloss
+fi
+
+if [ ! -d "epics" ] | [ ! -e "epics" ]; then
+        ln -s /usr/local/clas-software/reconstruction/epics
+fi
+
+if [ ! -d "fdump" ] | [ ! -e "fdump" ]; then
+        ln -s /usr/local/clas-software/io/fdump
+fi
+
+if [ ! -d "fpack" ] | [ ! -e "fpack" ]; then
+        ln -s /usr/local/clas-software/io/fpack
+fi
+
+if [ ! -d "fputil" ] | [ ! -e "fputil" ]; then
+        ln -s /usr/local/clas-software/io/fputil
+fi
+
+if [ ! -d "gem" ] | [ ! -e "gem" ]; then
+        ln -s /usr/local/clas-software/reconstruction/gem
+fi
+
+if [ ! -d "gpp" ] | [ ! -e "gpp" ]; then
+        ln -s /usr/local/clas-software/build/bin/gpp
+fi
+
+if [ ! -d "gsim" ] | [ ! -e "gsim" ]; then
+        ln -s /usr/local/clas-software/simulation/gsim
+fi
+
+if [ ! -d "gsim_bat" ] | [ ! -e "gsim_bat" ]; then
+        ln -s /usr/local/clas-software/build/bin/gsim_bat
+fi
+
+if [ ! -d "gsim_int" ] | [ ! -e "gsim_int" ]; then
+        ln -s /usr/local/clas-software/build/bin/gsim_int
+fi
+
+if [ ! -d "Hv" ] | [ ! -e "Hv" ]; then
+        ln -s /usr/local/clas-software/analysis/ced/Hv
+fi
+
+if [ ! -d "icf" ] | [ ! -e "icf" ]; then
+        ln -s /usr/local/clas-software/detector/icf
+fi
+
+if [ ! -d "include" ] | [ ! -e "include" ]; then
+        ln -s /usr/local/clas-software/analysis/ClasTool/include
+fi
+
+if [ ! -d "lac" ] | [ ! -e "lac" ]; then
+        ln -s /usr/local/clas-software/detector/lac
+fi
+
+if [ ! -d "Makefile" ] | [ ! -e "Makefile" ]; then
+        ln -s /usr/local/clas-software/analysis/ClasTool/Makefile
+fi
+
+if [ ! -d "Map" ] | [ ! -e "Map" ]; then
+        ln -s /usr/local/clas-software/io/Map
+fi
+
+####if [ ! -d "n10maker" ] | [ ! -e "n10maker" ]; then
+####        ln -s /usr/local/clas-software/build/bin/n10maker
+####fi
+
+if [ ! -d "online_dummy" ] | [ ! -e "online_dummy" ]; then
+        ln -s /usr/local/clas-software/reconstruction/online_dummy
+fi
+
+if [ ! -d "pdu" ] | [ ! -e "pdu" ]; then
+        ln -s /usr/local/clas-software/build/bin/pdu
+fi
+
+if [ ! -d "pid" ] | [ ! -e "pid" ]; then
+        ln -s /usr/local/clas-software/reconstruction/pid
+fi
+
+if [ ! -d "recsis" ] | [ ! -e "recsis" ]; then
+        ln -s /usr/local/clas-software/reconstruction/recsis
+fi
+
+if [ ! -d "scaler" ] | [ ! -e "scaler" ]; then
+        ln -s /usr/local/clas-software/reconstruction/scaler
+fi
+
+if [ ! -d "scat" ] | [ ! -e "scat" ]; then
+        ln -s /usr/local/clas-software/reconstruction/scat
+fi
+
+if [ ! -d "scripts" ] | [ ! -e "scripts" ]; then
+        ln -s /usr/local/clas-software/analysis/ClasTool/scripts
+fi
+
+if [ ! -d "seb" ] | [ ! -e "seb" ]; then
+        ln -s /usr/local/clas-software/reconstruction/seb
+fi
+
+if [ ! -d "sh" ] | [ ! -e "sh" ]; then
+        ln -s /usr/local/clas-software/detector/sh
+fi
+
+if [ ! -d "st" ] | [ ! -e "st" ]; then
+        ln -s /usr/local/clas-software/detector/st
+fi
+
+if [ ! -d "tag" ] | [ ! -e "tag" ]; then
+        ln -s /usr/local/clas-software/detector/tag
+fi
+
+if [ ! -d "trk" ] | [ ! -e "trk" ]; then
+        ln -s /usr/local/clas-software/reconstruction/trk
+fi
+
+if [ ! -d "user" ] | [ ! -e "user" ]; then
+        ln -s /usr/local/clas-software/reconstruction/user
+fi
+
+if [ ! -d "user_ana" ] | [ ! -e "user_ana" ]; then
+        ln -s /usr/local/clas-software/build/bin/user_ana
+fi
+
+if [ ! -d "utilities" ] | [ ! -e "utilities" ]; then
+        ln -s /usr/local/clas-software/io/Map/utilities
+fi
+
+if [ ! -d "vertex" ] | [ ! -e "vertex" ]; then
+        ln -s /usr/local/clas-software/reconstruction/vertex
+fi
+
